560470a69916b7b86c15c5d5830d5565b1509af1
faridabderrahmani <abderrahmani@yandex.ru>
2023-12-27 22:18:59 +0100

    printLn fixed

--- src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("I'm updated " +
+        System.out.println("I'm updated "+
                 "\ncan be compiled. And can write to console");
     }
 }
